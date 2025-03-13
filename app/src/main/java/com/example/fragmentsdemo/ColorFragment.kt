4234d2e9acf80def9843d370a22a8e7badef91ca
plasticono <friedboy@comcast.net>
2025-03-13 11:34:18 -0400

    completed

--- app/src/main/java/com/example/fragmentsdemo/ColorFragment.kt
+++ app/src/main/java/com/example/fragmentsdemo/ColorFragment.kt
@@ -1,10 +1,13 @@
 package com.example.fragmentsdemo
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
@@ -15,10 +18,33 @@ class ColorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_color, container, false).apply {
+        return inflater.inflate(R.layout.fragment_color, container, false)
+    }
+
+
+    override fun onViewCreated(v: View, savedInstanceState: Bundle?) {
+        v.apply {
             findViewById<Spinner>(R.id.spinner).apply {
-                adapter = ArrayAdapter(this.context, android.R.layout.simple_spinner_dropdown_item,
+                adapter = ArrayAdapter(
+                    this.context, android.R.layout.simple_spinner_dropdown_item,
                     arrayOf("Red", "Green", "Blue"))
+                onItemSelectedListener = object : OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        parent?.run {
+                            v.setBackgroundColor(Color.parseColor(getItemAtPosition(position).toString()))
+                        }
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
+
+                }
             }
         }
     }
